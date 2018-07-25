d3a72b1db3e384a4b2d37da8ff60fecc5f393372
Leslie Parba <lparba@gmail.com>
2018-07-25 15:11:33 +0400

    adding CCS Transition using ReactCSSTransitionGroup

--- csstask/server.js
+++ csstask/server.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var app = express();
 
-//Set Port
+// Set Port 
 app.set('port', (process.env.PORT || 3000));
 
-// Set Static path
+// Set Static path 
 app.use(express.static(path.join(__dirname, 'client')));
 
-// BodyParser Middleware
-app.use(bodyParser.json());
+// BodyParser Middleware 
+app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Start Server
+// start Server 
 app.listen(app.get('port'), function(){
-	console.log('Server Has Started On Port: '+ app.get('port'));
+	console.log('Server Has Started on Port: '+app.get('port'));
 });

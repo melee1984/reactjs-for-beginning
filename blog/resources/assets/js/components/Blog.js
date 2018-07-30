f56b715dc02622207d9cab9acf223a7f3c2f2c55
Leslie Parba <lparba@gmail.com>
2018-07-30 17:37:51 +0400

    adding blog Laravel5.6+react

---
+++ blog/resources/assets/js/components/Blog.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+export default class Blog extends Component {
+
+    constructor() {
+
+        super();
+        this.state = {
+            blogs: []
+        }
+    }
+
+    componentWillMount() {
+
+        axios.get('/api/blog').then(response => {
+            this.setState({
+                blogs: response.data
+            });
+        }).catch(errors => {
+            console.log(errors)
+        });
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <ul>  
+               {this.state.blogs.map(blog => <li>{blog.body}</li>)}
+               </ul>
+            </div>
+        );
+    }
+}
+
+if (document.getElementById('example')) {
+    ReactDOM.render(<Blog />, document.getElementById('example'));
+}
